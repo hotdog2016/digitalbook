578e77a6ee9f1d7b28b089595d3b3a1b19bfa697
hotdog2016 <yujinxing2013@126.com>
2016-11-13 00:33:02 +0800

    2016-11-13 00:33:02

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -37,7 +37,7 @@ flags = [
 '-std=c99',
 # ...and the same thing goes for the magic -x option which specifies the
 # language that the files to be compiled are written in. This is mostly
-# relevant for c++ headers.
+# relevant for c++ headers.***********************************************
 # For a C project, you would set this to 'c' instead of 'c++'.
 '-x',
 'c',
